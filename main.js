586506aa7f24b729ed58458c226e40a7cb531a4e
Frank Fang <FrankFang1990@gmail.com>
2017-11-17 21:53:48 +0800

    触屏设备兼容

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var context = yyy.getContext('2d');
 
 autoSetCanvasSize(yyy)
 
-listenToMouse(yyy)
+listenToUser(yyy)
 
 
 var eraserEnabled = false
@@ -53,7 +53,7 @@ function drawLine(x1, y1, x2, y2) {
   context.closePath()
 }
 
-function listenToMouse(canvas) {
+function listenToUser(canvas) {
 
 
   var using = false
@@ -61,38 +61,81 @@ function listenToMouse(canvas) {
     x: undefined,
     y: undefined
   }
-  canvas.onmousedown = function(aaa) {
-    var x = aaa.clientX
-    var y = aaa.clientY
-    using = true
-    if (eraserEnabled) {
-      context.clearRect(x - 5, y - 5, 10, 10)
-    } else {
-      lastPoint = {
-        "x": x,
-        "y": y
+  // 特性检测
+  if(document.body.ontouchstart !== undefined){
+    // 触屏设备 苏菲就是个触屏设备啊哥
+    canvas.ontouchstart = function(aaa){
+      var x = aaa.touches[0].clientX
+      var y = aaa.touches[0].clientY
+      console.log(x,y)
+      using = true
+      if (eraserEnabled) {
+        context.clearRect(x - 5, y - 5, 10, 10)
+      } else {
+        lastPoint = {
+          "x": x,
+          "y": y
+        }
       }
     }
-  }
-  canvas.onmousemove = function(aaa) {
-    var x = aaa.clientX
-    var y = aaa.clientY
+    canvas.ontouchmove = function(aaa){
+      console.log('边摸边动')
+      var x = aaa.touches[0].clientX
+      var y = aaa.touches[0].clientY
 
-    if (!using) {return}
+      if (!using) {return}
 
-    if (eraserEnabled) {
-      context.clearRect(x - 5, y - 5, 10, 10)
-    } else {
-      var newPoint = {
-        "x": x,
-        "y": y
+      if (eraserEnabled) {
+        context.clearRect(x - 5, y - 5, 10, 10)
+      } else {
+        var newPoint = {
+          "x": x,
+          "y": y
+        }
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
       }
-      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-      lastPoint = newPoint
     }
+    canvas.ontouchend = function(){
+      console.log('摸完了')
+      using = false
+    }
+  }else{
+    // 非触屏设备
+    canvas.onmousedown = function(aaa) {
+      var x = aaa.clientX
+      var y = aaa.clientY
+      using = true
+      if (eraserEnabled) {
+        context.clearRect(x - 5, y - 5, 10, 10)
+      } else {
+        lastPoint = {
+          "x": x,
+          "y": y
+        }
+      }
+    }
+    canvas.onmousemove = function(aaa) {
+      var x = aaa.clientX
+      var y = aaa.clientY
 
+      if (!using) {return}
+
+      if (eraserEnabled) {
+        context.clearRect(x - 5, y - 5, 10, 10)
+      } else {
+        var newPoint = {
+          "x": x,
+          "y": y
+        }
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+
+    }
+    canvas.onmouseup = function(aaa) {
+      using = false
+    }
   }
-  canvas.onmouseup = function(aaa) {
-    using = false
-  }
+
 }
